3341c019ed64ad0aaf9c23e2fe0bd2a84e7b263e
Douglas Maringa <douglasmaringa4@gmail.com>
2020-09-22 18:08:33 +0200

    Update todo.model.js

--- todo.model.js
+++ todo.model.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 let Todo = new Schema({
-  todoDescription: {
-        type: String
-    }
-})
+    name: {
+          type: String
+      },
+      post: {
+          type: String
+      }
+  })
+  
 
 module.exports = mongoose.model('Todo', Todo)
